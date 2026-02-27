70a74605666c61c7a5f47b674b94fd306fe48363
Jose Quiceno <josequiceno000@gmail.com>
2026-02-27 12:35:44 -0600

    Add my_each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,13 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    for i in 0...self.length
+      yield(self[i])
+    end
+
+    self
+  end
 end
